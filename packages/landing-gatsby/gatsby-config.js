af694ea746d29c855041abcb8df618935da22c12
Haoyang Chen <haoyangchen0901@gmail.com>
2021-04-30 16:18:55 -0700

    Fix Font Issue

--- packages/landing-gatsby/gatsby-config.js
+++ packages/landing-gatsby/gatsby-config.js
@@ -49,7 +49,7 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-google-fonts`,
+      resolve: `gatsby-plugin-web-font-loader`,
       options: {
         fonts: [
           {
